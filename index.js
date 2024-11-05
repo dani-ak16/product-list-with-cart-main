bebf0abc92096b33d5435d113f2a859da937cf36
dani-ak16 <dannyak16@gmail.com>
2024-11-05 10:55:37 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import data from "./data.json" with { type: 'json'}
+// import data from "./data.json" with { type: 'json'}
 
 const cardContainer = document.getElementById("card-container")
 const cartQuantity = document.getElementById("cart-quantity")
@@ -37,54 +37,62 @@ function getImageForScreenSize(images) {
     }
 }
 
-const render = () => {
-    
-    for (let i = 0; i < data.length; i++){
-
-        imgSrc = getImageForScreenSize(data[i].image)
-
-        
-        cards += `
-        
-        <div class="prodcut-card flex-column">
-            <div class="img-container" id="img-container">
-                <img 
-                    src=${imgSrc} 
-                    
-                    alt=""/>
-                    
-                <button class="add-to-cart" id="product-${i}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
-                    <p>Add to Cart</p>
-                </button>
-
-                <div class="active-btn">
-                    <button class="decrement-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path id="svg-btn" fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
-                    </button>
-                    <span class="current-quantity"></span>
-                    <button class="increment-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path id="svg-btn" fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
-                    </button>
-                </div>
-
-            </div>
-            <div class="product-details" id="product-details">
-                <p>${data[i].category}</p>
-                <h3>${data[i].name}</h3>
-                <span>$${data[i].price.toFixed(2)}</span>
-            </div>
-        </div>   
-        `
-        itemCountList.push([])
+// Fetch the JSON data and then render the page
+fetch('./data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
     }
-     
-    cardContainer.innerHTML = cards
-}
+    return response.json(); // Convert response to JSON
+  })
+  .then(data => {
+    // Pass the fetched data to the render function
+    render(data);
+  })
+  .catch(error => console.error('Error loading JSON data:', error));
 
-render()
+// Render function that accepts data as a parameter
+const render = (data) => {
+  let cards = '';
+  
+  for (let i = 0; i < data.length; i++) {
+    const imgSrc = getImageForScreenSize(data[i].image); // Get image based on screen size
 
-const renderCart = () => {
+    // Generate HTML for each product
+    cards += `
+      <div class="product-card flex-column">
+          <div class="img-container" id="img-container">
+              <img src="${imgSrc}" alt=""/>
+              
+              <button class="add-to-cart" id="product-${i}">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg>
+                  <p>Add to Cart</p>
+              </button>
+
+              <div class="active-btn">
+                  <button class="decrement-btn">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path id="svg-btn" fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>
+                  </button>
+                  <span class="current-quantity"></span>
+                  <button class="increment-btn">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path id="svg-btn" fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
+                  </button>
+              </div>
+          </div>
+          <div class="product-details" id="product-details">
+              <p>${data[i].category}</p>
+              <h3>${data[i].name}</h3>
+              <span>$${data[i].price.toFixed(2)}</span>
+          </div>
+      </div>   
+    `
+    itemCountList.push([])
+    ;
+  }
+
+  cardContainer.innerHTML = cards;
+
+  const renderCart = () => {
     if (cartCount === 0){
         emptyCartCard.style.display  = "flex"
         orderDetailsContainer.style.display = "none"
@@ -293,6 +301,10 @@ newOrder.addEventListener("click", function(){
 
     renderCart()
 })
+};
+
+
+
 
 
 
